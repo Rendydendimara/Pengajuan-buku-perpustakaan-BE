dbb9a66749d05824058df4c76794cb62091cd5cf
Waingapucoder <r3ndydinar@gmail.com>
2023-10-10 07:59:56 +0800

    feat: lastest

--- src/usecase/Shared/index.ts
+++ src/usecase/Shared/index.ts
@@ -17,21 +17,45 @@ export const getStatistikSharedUseCase = async (
       const totalBuku = await Buku.countDocuments({ deletedAt: null });
       const totalKatalog = await KatalogBuku.countDocuments({
         deletedAt: null,
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
       });
       const totalPengajuanBuku = await PengajuanBuku.countDocuments({
         deletedAt: null,
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
       });
       const totalPengajuanBukuSelesai = await PengajuanBuku.countDocuments({
         status: 'selesai',
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
       });
       const totalPengajuanBukuGagal = await PengajuanBuku.countDocuments({
         status: 'ditolak',
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
       });
       const totalPengajuanBukuDalamProses = await PengajuanBuku.countDocuments({
         $or: [{ status: 'diterima' }, { status: 'diproses' }],
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
       });
       const dataPengajuanBuku = await PengajuanBuku.find({
         deletedAt: null,
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
       }).select('status createdAt');
 
       return res.send({
@@ -51,28 +75,63 @@ export const getStatistikSharedUseCase = async (
       const totalBuku = await Buku.countDocuments({
         deletedAt: null,
         prodi: type,
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
+      });
+      console.log({
+        deletedAt: null,
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
       });
       const totalKatalog = await KatalogBuku.countDocuments({
         deletedAt: null,
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
       });
       const totalPengajuanBuku = await PengajuanBuku.countDocuments({
         deletedAt: null,
         dosenProdi: new mongoose.Types.ObjectId(adminProdi),
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
       });
       const totalPengajuanBukuSelesai = await PengajuanBuku.countDocuments({
         status: 'selesai',
         dosenProdi: new mongoose.Types.ObjectId(adminProdi),
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
       });
       const totalPengajuanBukuGagal = await PengajuanBuku.countDocuments({
         status: 'ditolak',
         dosenProdi: new mongoose.Types.ObjectId(adminProdi),
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
       });
       const totalPengajuanBukuDalamProses = await PengajuanBuku.countDocuments({
         $or: [{ status: 'diterima' }, { status: 'diproses' }],
         dosenProdi: new mongoose.Types.ObjectId(adminProdi),
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
       });
       const dataPengajuanBuku = await PengajuanBuku.find({
         deletedAt: null,
+        createdAt: {
+          $gte: new Date(new Date().getFullYear(), 0, 31),
+          $lt: new Date(new Date().getFullYear(), 12, 31),
+        },
         dosenProdi: new mongoose.Types.ObjectId(adminProdi),
       }).select('status createdAt');
 
